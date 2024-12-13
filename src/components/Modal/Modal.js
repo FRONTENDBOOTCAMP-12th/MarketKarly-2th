f35b0a3ab8ce5042ca532c001ad59a4719f15c93
llhyeon <0617mosses@gmail.com>
2024-12-13 16:09:42 +0900

    [Feat] Modal '오늘 하루 보지 않기' 기능 구현
    - '오늘 하루 보지 않기' 기능 구현 중
    
    Resolved

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -3,8 +3,41 @@ import style from '@/components/Modal/Modal.css?inline';
 import '@/assets/font/Pretendard.css';
 
 class Modal extends LitElement {
+  static properties = {
+    isLongClosed: {},
+  };
+
   constructor() {
     super();
+    this.isLongClosed = false;
+  }
+  connectedCallback() {
+    super.connectedCallback();
+  }
+
+  get buttons() {
+    return this.renderRoot.querySelectorAll('button');
+  }
+
+  get modal() {
+    return this.renderRoot.querySelector('.modal');
+  }
+
+  handleShortClose() {
+    const shortClose = this.buttons[1];
+    const modal = shortClose.closest('section');
+    modal.classList.add('close');
+    this.isLongClosed = false;
+    localStorage.setItem('isLongClosed', this.isLongClosed);
+  }
+
+  handleLongClose() {
+    const longClose = this.buttons[0];
+    const modal = longClose.closest('section');
+    modal.classList.add('close');
+    this.isLongClosed = true;
+
+    localStorage.setItem('isLongClosed', this.isLongClosed);
   }
 
   render() {
@@ -23,8 +56,10 @@ class Modal extends LitElement {
           </p>
         </div>
         <div class="button-wrapper">
-          <button type="button">오늘 하루 안 보기</button>
-          <button type="button">닫기</button>
+          <button @click=${this.handleLongClose} type="button">
+            오늘 하루 안 보기
+          </button>
+          <button @click=${this.handleShortClose} type="button">닫기</button>
         </div>
       </section>
     `;
