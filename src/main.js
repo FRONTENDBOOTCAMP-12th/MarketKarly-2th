b410cc40393bfa68cd08b44ce25d1e5697aa9626
blue31 <minijh98@gmail.com>
2024-12-18 15:02:52 +0900

    [Modify] PR 날리기 위한 세팅
    - 컴포넌트 추가했던 거 삭제제
    
    Resolved #45

--- src/main.js
+++ src/main.js
@@ -4,5 +4,3 @@ import '@/components/Banner';
 import '@/components/RecentProduct';
 import '@/pages/main/main';
 import '@/components/Modal';
-import '@/components/ReviewModal';
-import '@/components/InquiryModal';
